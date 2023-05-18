5f624c04262e533d32fd883d7664808441bd01f5
Elijah Obominuru <elijahobominuru@gmail.com>
2023-05-18 19:33:53 +0100

    updates

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,13 +1,7 @@
-import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
 import React, { useLayoutEffect } from "react";
+import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-
-import {
-  UserIcon,
-  ChevronDownIcon,
-  SearchIcon,
-  AdjustmentsIcon,
-} from "react-native-heroicons/outline";
+import { UserIcon, ChevronDownIcon } from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -16,45 +10,42 @@ const HomeScreen = () => {
     navigation.setOptions({
       headerShown: false,
     });
-  }, []);
+  }, [navigation]);
 
   return (
     <SafeAreaView className='bg-white'>
-      <Text className='text-red-500'>
-        {/* Header */}
-        <View className='flex-row pb-3 items-center mx-4 space-x-2'>
-          <Image
-            source={{
-              uri: "https://links.papareact.com/wru",
-            }}
-            className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+      {/* Header */}
+      <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+        />
+
+        <View className='flex-1'>
+          <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
+          <Text className='font-bold text-xl'>
+            Current Location
+            <ChevronDownIcon size={20} color='#00CCBB' />
+          </Text>
+        </View>
+
+        <UserIcon size={35} color='#00CCBB' />
+      </View>
+
+      {/* Search */}
+      <View>
+        <View className='flex-row space-x-2 bg-gray-200 p-3'>
+          {/* <SearchIcon /> */}
+          <TextInput
+            placeholder='Restaurant and cuisines'
+            keyboardType='default'
           />
-
-          <View className='flex-1'>
-            <Text className='font-bold text-gray-400 text-xs'>
-              Deliver Now!
-            </Text>
-            <Text className='font-bold text-xl'>
-              Current Location <ChevronDownIcon size={20} color='#00CCBB' />
-            </Text>
-          </View>
-
-          <UserIcon size={35} color='#00CCBB' />
         </View>
 
-        {/* Search */}
-        <View>
-          <View className='flex-row space-x-2 bg-gray-200 p-3'>
-            {/* <SearchIcon /> */}
-            <TextInput
-              placeholder='Restaurant and cuisines'
-              keyboardType='default'
-            />
-          </View>
-
-          {/* <AdjustmentsIcon color="#00CCBB" /> */}
-        </View>
-      </Text>
+        {/* <AdjustmentsIcon color="#00CCBB" /> */}
+      </View>
     </SafeAreaView>
   );
 };
