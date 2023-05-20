40b3afbbf6c6e02230a97e0d376d23b1a193c6a8
Elijah Obominuru <elijahobominuru@gmail.com>
2023-05-21 00:53:22 +0100

    updates

--- components/Feautured.js
+++ components/Feautured.js
@@ -5,19 +5,14 @@ import { ArrowRightIcon } from "react-native-heroicons/outline";
 const Featured = ({ id, title, description }) => {
   return (
     <View>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginTop: 20,
-          paddingHorizontal: 15,
-        }}
+      <View className='flex-row items-center justify-between px-4 mt-4'
       >
-        <Text style={{ fontWeight: "bold", fontSize: 18 }}>{title}</Text>
+        <Text className='font-bold text-lg'>{title}</Text>
         <ArrowRightIcon size={20} color='#00CCBB' />
       </View>
-      {/* <Text>{description}</Text> */}
+      <Text
+      className='text-gray-500 text-sm px-4'
+      >{description}</Text>
     </View>
   );
 };
