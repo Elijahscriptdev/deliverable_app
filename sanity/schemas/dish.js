e21ebb11783f8900d7401fce20931a602740e10d
Elijah Obominuru <elijahobominuru@gmail.com>
2023-05-21 03:11:43 +0100

    updates

---
+++ sanity/schemas/dish.js
@@ -0,0 +1,38 @@
+export default {
+  name: 'dish',
+  title: 'Dish',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Dish Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'short_description',
+      title: 'Short Description',
+      type: 'string',
+      validation: (Rule) => Rule.max(200),
+    },
+    {
+      name: 'image',
+      title: 'Image of Dish',
+      type: 'image',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'reference',
+      to: [{ type: 'category' }],
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+}
---
+++ sanity/schemas/restaurant.js
@@ -0,0 +1,78 @@
+export default {
+  name: 'restaurant',
+  title: 'Restaurant',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Restaurant Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'short_description',
+      title: 'Short Description',
+      type: 'string',
+      validation: (Rule) => Rule.max(200),
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'address',
+      title: 'Address',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'phone',
+      title: 'Phone',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'website',
+      title: 'Website',
+      type: 'url',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'lat',
+      title: 'Latitude',
+      type: 'number',
+    },
+    {
+      name: 'lng',
+      title: 'Longitude',
+      type: 'number',
+    },
+    {
+      name: 'rating',
+      title: 'Rating',
+      type: 'number',
+      validation: (Rule) => Rule.min(1).max(5).error('Rating must be between 1 and 5'),
+    },
+    {
+      name: 'type',
+      title: 'Category',
+      validation: (Rule) => Rule.required(),
+      type: 'reference',
+      to: [{type: 'category'}],
+    },
+    {
+      name: 'dishes',
+      title: 'Dishes',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'dish'}]}],
+    },
+    {
+      name: 'delivery_time',
+      title: 'Delivery Time',
+      type: 'number',
+      validation: (Rule) => Rule.required(),
+    },
+  ],
+}
